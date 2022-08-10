9429b1a4b9fe322cc5b7eab62aa0502df13bacde
pvillor <pvillor@gmail.com>
2022-08-10 11:40:46 -0300

    feat/diagram

---
+++ animals/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class SexAnimal(models.TextChoices):
+    MACHO = "Macho"
+    FEMEA = "Fêmea"
+    NAO_INFORMADO = "Não informado"
+
+class Animal(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=15,
+        choices=SexAnimal.choices,
+        default=SexAnimal.NAO_INFORMADO
+        )
+    group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="animals")
+
+    def __repr__(self) -> str:
+        return f'Animal {self.id} - {self.name}'
